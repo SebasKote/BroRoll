0a8470c66d65ec21c0c0bd8980281ec56d12aaef
SebasKote <sebastianolivera386@gmail.com>
2022-06-10 01:12:58 -0500

    Change the links
    One in mp4 and one in WEBm

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ const DELAY_IN_MS = 3000;
 const VIDEO_LENGTH_IN_MS = 20000;
 
 const VIDEO_HTML_STRING =
-  "<video autoplay loop style='height: 100vh; width: 100vw'><source src='https://raw.githubusercontent.com/Roystbeef/Cenafy/master/cena.webm' type='video/webm'><source src='https://raw.githubusercontent.com/Roystbeef/Cenafy/master/cena.mp4' type='video/mp4'></video>";
+  "<video autoplay loop style='height: 100vh; width: 100vw'><source src='https://github.com/SebasKote/BroRoll/blob/main/Broroll.webm' type='video/webm'><source src='https://github.com/SebasKote/BroRoll/blob/main/Broroll.mp4' type='video/mp4'></video>";
 
 function broroll() {
   setTimeout(() => {
